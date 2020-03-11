94e7d052866770506994e97df7fef845684d226c
hadisutanto <61482945+hadisutanto@users.noreply.github.com>
2020-03-11 20:16:38 +0700

    update asset_registration controller
    upload dari hadi - comment dari index function

--- ASMSolution/Controllers/asset_registrationController.cs
+++ ASMSolution/Controllers/asset_registrationController.cs
@@ -28,140 +28,140 @@ namespace ASM_UI.Controllers
         public ActionResult Index()
         {
             //var _list = db.tr_asset_registration.Where(a => a.fl_active == true && a.deleted_date == null).ToList();
-            var _qry = new object();
-            if (UserProfile.asset_reg_location_id == 2)
-            {
-                _qry = (from ar in db.tr_asset_registration
-                        where (ar.fl_active == true && ar.deleted_date == null
-                        && ar.company_id == UserProfile.company_id
-                        //&& ar.asset_reg_location_id == UserProfile.asset_reg_location_id 
-                        && ar.current_location_id == UserProfile.location_id
-                        && ar.asset_type_id == (int)Enum_asset_type_Key.AssetParent)
+            //var _qry = new object();
+            //if (UserProfile.asset_reg_location_id == 2)
+            //{
+            //    _qry = (from ar in db.tr_asset_registration
+            //            where (ar.fl_active == true && ar.deleted_date == null
+            //            && ar.company_id == UserProfile.company_id
+            //            //&& ar.asset_reg_location_id == UserProfile.asset_reg_location_id 
+            //            && ar.current_location_id == UserProfile.location_id
+            //            && ar.asset_type_id == (int)Enum_asset_type_Key.AssetParent)
 
-                        join a in db.ms_vendor on ar.vendor_id equals a.vendor_id
-                        where (a.fl_active == true && a.deleted_date == null)
+            //            join a in db.ms_vendor on ar.vendor_id equals a.vendor_id
+            //            where (a.fl_active == true && a.deleted_date == null)
 
-                        join b in db.ms_asset_register_location on ar.asset_reg_location_id equals b.asset_reg_location_id
-                        where (b.fl_active == true && b.deleted_date == null)
+            //            join b in db.ms_asset_register_location on ar.asset_reg_location_id equals b.asset_reg_location_id
+            //            where (b.fl_active == true && b.deleted_date == null)
 
-                        join c in db.ms_asset_register_pic on ar.asset_reg_pic_id equals c.asset_reg_pic_id
-                        where (c.fl_active == true && c.deleted_date == null)
+            //            join c in db.ms_asset_register_pic on ar.asset_reg_pic_id equals c.asset_reg_pic_id
+            //            where (c.fl_active == true && c.deleted_date == null)
 
-                        join d in db.ms_asset_type on ar.asset_type_id equals d.activa_type_id
-                        where (d.fl_active == true && d.deleted_date == null)
+            //            join d in db.ms_asset_type on ar.asset_type_id equals d.activa_type_id
+            //            where (d.fl_active == true && d.deleted_date == null)
 
-                        join e in db.ms_asset_category on ar.category_id equals e.category_id
-                        where (e.fl_active == true && e.deleted_date == null)
+            //            join e in db.ms_asset_category on ar.category_id equals e.category_id
+            //            where (e.fl_active == true && e.deleted_date == null)
 
-                        join f in db.ms_asmin_company on ar.company_id equals f.company_id
-                        where (f.fl_active == true && f.deleted_date == null)
+            //            join f in db.ms_asmin_company on ar.company_id equals f.company_id
+            //            where (f.fl_active == true && f.deleted_date == null)
 
-                        join g in db.ms_department on ar.department_id equals g.department_id
-                        where (g.fl_active == true && g.deleted_date == null)
+            //            join g in db.ms_department on ar.department_id equals g.department_id
+            //            where (g.fl_active == true && g.deleted_date == null)
 
-                        join h in db.ms_employee on ar.employee_id equals h.employee_id
-                        where (h.fl_active == true && h.deleted_date == null)
+            //            join h in db.ms_employee on ar.employee_id equals h.employee_id
+            //            where (h.fl_active == true && h.deleted_date == null)
 
-                        join i in db.ms_asset_location on ar.location_id equals i.location_id
-                        where (i.fl_active == true && i.deleted_date == null)
+            //            join i in db.ms_asset_location on ar.location_id equals i.location_id
+            //            where (i.fl_active == true && i.deleted_date == null)
 
-                        select new asset_registrationViewModel()
-                        {
-                            asset_id = ar.asset_id,
-                            asset_type_id = ar.asset_type_id,
-                            asset_type = d,
-                            asset_number = ar.asset_number,
-                            company_id = ar.company_id,
-                            company = f,
-                            asset_reg_location_id = ar.asset_reg_location_id,
-                            asset_reg_location = b,
-                            asset_reg_pic_id = ar.asset_reg_pic_id,
-                            asset_reg_pic = c,
-                            category_id = ar.category_id,
-                            asset_category = e,
-                            asset_po_number = ar.asset_po_number,
-                            asset_do_number = ar.asset_do_number,
-                            asset_name = ar.asset_name,
-                            asset_merk = ar.asset_merk,
-                            asset_serial_number = ar.asset_serial_number,
-                            vendor_id = ar.vendor_id,
-                            vendor = a,
-                            asset_receipt_date = ar.asset_receipt_date,
-                            location_id = ar.location_id,
-                            asset_location = i,
-                            department_id = ar.department_id,
-                            department = g,
-                            employee_id = ar.employee_id,
-                            employee = h,
-                            asset_description = ar.asset_description
-                        }).ToList<asset_registrationViewModel>();
-            }
-            else if (UserProfile.asset_reg_location_id == 1)
-            {
-                _qry = (from ar in db.tr_asset_registration
-                        where (ar.fl_active == true && ar.deleted_date == null
-                        && ar.company_id == UserProfile.company_id
-                        && ar.asset_type_id == (int)Enum_asset_type_Key.AssetParent)
+            //            select new asset_registrationViewModel()
+            //            {
+            //                asset_id = ar.asset_id,
+            //                asset_type_id = ar.asset_type_id,
+            //                asset_type = d,
+            //                asset_number = ar.asset_number,
+            //                company_id = ar.company_id,
+            //                company = f,
+            //                asset_reg_location_id = ar.asset_reg_location_id,
+            //                asset_reg_location = b,
+            //                asset_reg_pic_id = ar.asset_reg_pic_id,
+            //                asset_reg_pic = c,
+            //                category_id = ar.category_id,
+            //                asset_category = e,
+            //                asset_po_number = ar.asset_po_number,
+            //                asset_do_number = ar.asset_do_number,
+            //                asset_name = ar.asset_name,
+            //                asset_merk = ar.asset_merk,
+            //                asset_serial_number = ar.asset_serial_number,
+            //                vendor_id = ar.vendor_id,
+            //                vendor = a,
+            //                asset_receipt_date = ar.asset_receipt_date,
+            //                location_id = ar.location_id,
+            //                asset_location = i,
+            //                department_id = ar.department_id,
+            //                department = g,
+            //                employee_id = ar.employee_id,
+            //                employee = h,
+            //                asset_description = ar.asset_description
+            //            }).ToList<asset_registrationViewModel>();
+            //}
+            //else if (UserProfile.asset_reg_location_id == 1)
+            //{
+            //    _qry = (from ar in db.tr_asset_registration
+            //            where (ar.fl_active == true && ar.deleted_date == null
+            //            && ar.company_id == UserProfile.company_id
+            //            && ar.asset_type_id == (int)Enum_asset_type_Key.AssetParent)
 
-                        join a in db.ms_vendor on ar.vendor_id equals a.vendor_id
-                        where (a.fl_active == true && a.deleted_date == null)
+            //            join a in db.ms_vendor on ar.vendor_id equals a.vendor_id
+            //            where (a.fl_active == true && a.deleted_date == null)
 
-                        join b in db.ms_asset_register_location on ar.asset_reg_location_id equals b.asset_reg_location_id
-                        where (b.fl_active == true && b.deleted_date == null)
+            //            join b in db.ms_asset_register_location on ar.asset_reg_location_id equals b.asset_reg_location_id
+            //            where (b.fl_active == true && b.deleted_date == null)
 
-                        join c in db.ms_asset_register_pic on ar.asset_reg_pic_id equals c.asset_reg_pic_id
-                        where (c.fl_active == true && c.deleted_date == null)
+            //            join c in db.ms_asset_register_pic on ar.asset_reg_pic_id equals c.asset_reg_pic_id
+            //            where (c.fl_active == true && c.deleted_date == null)
 
-                        join d in db.ms_asset_type on ar.asset_type_id equals d.activa_type_id
-                        where (d.fl_active == true && d.deleted_date == null)
+            //            join d in db.ms_asset_type on ar.asset_type_id equals d.activa_type_id
+            //            where (d.fl_active == true && d.deleted_date == null)
 
-                        join e in db.ms_asset_category on ar.category_id equals e.category_id
-                        where (e.fl_active == true && e.deleted_date == null)
+            //            join e in db.ms_asset_category on ar.category_id equals e.category_id
+            //            where (e.fl_active == true && e.deleted_date == null)
 
-                        join f in db.ms_asmin_company on ar.company_id equals f.company_id
-                        where (f.fl_active == true && f.deleted_date == null)
+            //            join f in db.ms_asmin_company on ar.company_id equals f.company_id
+            //            where (f.fl_active == true && f.deleted_date == null)
 
-                        join g in db.ms_department on ar.department_id equals g.department_id
-                        where (g.fl_active == true && g.deleted_date == null)
+            //            join g in db.ms_department on ar.department_id equals g.department_id
+            //            where (g.fl_active == true && g.deleted_date == null)
 
-                        join h in db.ms_employee on ar.employee_id equals h.employee_id
-                        where (h.fl_active == true && h.deleted_date == null)
+            //            join h in db.ms_employee on ar.employee_id equals h.employee_id
+            //            where (h.fl_active == true && h.deleted_date == null)
 
-                        join i in db.ms_asset_location on ar.location_id equals i.location_id
-                        where (i.fl_active == true && i.deleted_date == null)
+            //            join i in db.ms_asset_location on ar.location_id equals i.location_id
+            //            where (i.fl_active == true && i.deleted_date == null)
 
-                        select new asset_registrationViewModel()
-                        {
-                            asset_id = ar.asset_id,
-                            asset_type_id = ar.asset_type_id,
-                            asset_type = d,
-                            asset_number = ar.asset_number,
-                            company_id = ar.company_id,
-                            company = f,
-                            asset_reg_location_id = ar.asset_reg_location_id,
-                            asset_reg_location = b,
-                            asset_reg_pic_id = ar.asset_reg_pic_id,
-                            asset_reg_pic = c,
-                            category_id = ar.category_id,
-                            asset_category = e,
-                            asset_po_number = ar.asset_po_number,
-                            asset_do_number = ar.asset_do_number,
-                            asset_name = ar.asset_name,
-                            asset_merk = ar.asset_merk,
-                            asset_serial_number = ar.asset_serial_number,
-                            vendor_id = ar.vendor_id,
-                            vendor = a,
-                            asset_receipt_date = ar.asset_receipt_date,
-                            location_id = ar.location_id,
-                            asset_location = i,
-                            department_id = ar.department_id,
-                            department = g,
-                            employee_id = ar.employee_id,
-                            employee = h,
-                            asset_description = ar.asset_description
-                        }).ToList<asset_registrationViewModel>();
-            }
-            return View(_qry);
+            //            select new asset_registrationViewModel()
+            //            {
+            //                asset_id = ar.asset_id,
+            //                asset_type_id = ar.asset_type_id,
+            //                asset_type = d,
+            //                asset_number = ar.asset_number,
+            //                company_id = ar.company_id,
+            //                company = f,
+            //                asset_reg_location_id = ar.asset_reg_location_id,
+            //                asset_reg_location = b,
+            //                asset_reg_pic_id = ar.asset_reg_pic_id,
+            //                asset_reg_pic = c,
+            //                category_id = ar.category_id,
+            //                asset_category = e,
+            //                asset_po_number = ar.asset_po_number,
+            //                asset_do_number = ar.asset_do_number,
+            //                asset_name = ar.asset_name,
+            //                asset_merk = ar.asset_merk,
+            //                asset_serial_number = ar.asset_serial_number,
+            //                vendor_id = ar.vendor_id,
+            //                vendor = a,
+            //                asset_receipt_date = ar.asset_receipt_date,
+            //                location_id = ar.location_id,
+            //                asset_location = i,
+            //                department_id = ar.department_id,
+            //                department = g,
+            //                employee_id = ar.employee_id,
+            //                employee = h,
+            //                asset_description = ar.asset_description
+            //            }).ToList<asset_registrationViewModel>();
+            //}
+            return View();
         }
 
         /// <summary>
