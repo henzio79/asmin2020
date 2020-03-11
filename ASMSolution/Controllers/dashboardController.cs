172ded6c05e038e8f0ba2a9e5164df835736a7b7
PardoDev <pardodev@users.noreply.github.com>
2020-03-11 21:45:13 +0700

    Tambah Class Dashboard
    Controller dan ViewModel

---
+++ ASMSolution/Controllers/dashboardController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASM_UI.Models;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using ASM_UI.App_Helpers;
+
+namespace ASM_UI.Controllers
+{
+    [Authorize]
+    public class dashboardController : BaseController
+    {
+        private ModelAsmRemote db = new ModelAsmRemote();
+
+        // GET: dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Dashboard_Chart1()
+        {
+            var _qry = new object();
+            try
+            {
+
+
+
+            }
+            catch { }
+            return Json(new { data = _qry }, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ ASMSolution/ViewModels/dashboardVIewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace ASM_UI.Models
+{
+    public class dashboard_chart01_ViewModel
+    {
+        public string department_code { get; set; }
+
+        public string asset_qty { get; set; }
+
+        public string asset_value { get; set; }
+
+    }
+
+
+    public class dashboard_chart02_ViewModel
+    {
+        public string category_code { get; set; }
+
+        public string asset_qty { get; set; }
+
+        public string asset_value { get; set; }
+
+    }
+
+    public class dashboard_chart03_ViewModel
+    {
+        public string site_code { get; set; }
+
+        public string asset_qty { get; set; }
+
+        public string asset_value { get; set; }
+
+    }
+
+
+}
